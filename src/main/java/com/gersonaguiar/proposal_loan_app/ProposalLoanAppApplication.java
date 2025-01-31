4dc3e6d953d1515d4cbaad0723721115f6e2ce6b
ggersonmg <gersoncafilho@gmail.com>
2025-01-31 13:22:17 -0300

    first commit

---
+++ src/main/java/com/gersonaguiar/proposal_loan_app/ProposalLoanAppApplication.java
@@ -0,0 +1,13 @@
+package com.gersonaguiar.proposal_loan_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProposalLoanAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProposalLoanAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gersonaguiar/proposal_loan_app/ProposalLoanAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gersonaguiar.proposal_loan_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProposalLoanAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
